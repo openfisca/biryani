8889e94d36bf5b65e2cd0b3935457309db8a07b5
Emmanuel Raviart <eraviart@easter-eggs.com>
2011-07-17 21:44:38 +0200

    Change license to Apache.

--- suq/converters.py
+++ suq/converters.py
@@ -4,23 +4,22 @@
 # Suq -- Python Toolbox
 # By: Emmanuel Raviart <eraviart@easter-eggs.com>
 #
-# Copyright (C) 2009, 2010 Easter-eggs
+# Copyright (C) 2009, 2010, 2011 Easter-eggs
 # http://wiki.infos-pratiques.org/wiki/Suq
 #
 # This file is part of Suq.
 #
-# Suq is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# Suq is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
+# http://www.apache.org/licenses/LICENSE-2.0
 #
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 
 """Conversion functions
