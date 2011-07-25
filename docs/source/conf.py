4997eac4a2b11a3ef47d236ac8ca1196b53a1acf
Emmanuel Raviart <eraviart@easter-eggs.com>
2011-07-25 23:18:09 +0200

    Add doctest extension and improve capitalization of project name.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.coverage', 'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -164,7 +164,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'BiryaniDoc'
+htmlhelp_basename = 'Biryanidoc'
 
 
 # -- Options for LaTeX output --------------------------------------------------
@@ -211,6 +211,6 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'Biryani', u'Biryani Documentation',
+    ('index', 'biryani', u'Biryani Documentation',
      [u'Emmanuel Raviart'], 1)
 ]
