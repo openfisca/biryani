96d05b907ed3b8616722e913bf1f4f4d51cc49e2
Emmanuel Raviart <eraviart@easter-eggs.com>
2011-08-02 10:59:10 +0200

    Ensure that french phone is always unicode after conversion from string.

--- biryani/frconv.py
+++ biryani/frconv.py
@@ -38,12 +38,20 @@ N_ = conv.N_
 
 
 def clean_str_to_phone(value, state = states.default_state):
-    """Convert a clean string to a phone number."""
+    """Convert a clean string to a phone number.
+
+    .. note:: For a converter that doesn't require a clean string, see :func:`str_to_phone`.
+
+    >>> clean_str_to_phone(u'0123456789')
+    (u'+33 1 23 45 67 89', None)
+    >>> clean_str_to_phone('0123456789')
+    (u'+33 1 23 45 67 89', None)
+    """
     if value is None:
         return value, None
     if value.startswith('+'):
         value = value.replace('+', '00', 1)
-    value = unicode(strings.slugify(value, separator = ''))
+    value = strings.slugify(value, separator = '')
     if not value:
         return value, None
     if not value.isdigit():
@@ -54,19 +62,19 @@ def clean_str_to_phone(value, state = states.default_state):
     if value.startswith('330'):
         value = value[2:]
     elif value.startswith('33'):
-        value = '0' + value[2:]
+        value = u'0' + value[2:]
 
-    if value.startswith('00'):
+    if value.startswith(u'00'):
         # International phone number
         country = {
-            '594': N_('French Guyana'),
-            '681': N_('Wallis and Futuna'),
-            '687': N_('New Caledonia'),
-            '689': N_('French Polynesia'),
+            u'594': N_('French Guyana'),
+            u'681': N_('Wallis and Futuna'),
+            u'687': N_('New Caledonia'),
+            u'689': N_('French Polynesia'),
             }.get(value[2:5])
         if country is not None:
             if len(value) == 11:
-                return '+{0} {1} {2} {3}'.format(value[2:5], value[5:7], value[7:9], value[9:11]), None
+                return u'+{0} {1} {2} {3}'.format(value[2:5], value[5:7], value[7:9], value[9:11]), None
             return value, state._('Wrong number of digits for phone number of {0}').format(_(country))
         return value, state._('Unknown international phone number')
     if len(value) == 4:
@@ -76,10 +84,16 @@ def clean_str_to_phone(value, state = states.default_state):
     if len(value) == 10:
         if value[0] != '0':
             return value, state._('Unexpected first digit in phone number: {0} instead of 0').format(value[0])
-        mask = '+33 {0}{1} {2} {3} {4}' if value[1] == '8' else '+33 {0} {1} {2} {3} {4}'
+        mask = u'+33 {0}{1} {2} {3} {4}' if value[1] == '8' else u'+33 {0} {1} {2} {3} {4}'
         return mask.format(value[1], value[2:4], value[4:6], value[6:8], value[8:10]), None
     return value, state._('Wrong number of digits in phone number')
 
 
 str_to_phone = conv.pipe(conv.cleanup_line, clean_str_to_phone)
+"""Convert a string to a phone number.
 
+    >>> str_to_phone(u'   0123456789   ')
+    (u'+33 1 23 45 67 89', None)
+    >>> str_to_phone('   0123456789   ')
+    (u'+33 1 23 45 67 89', None)
+    """
