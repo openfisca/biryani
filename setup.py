e146da1e9e347a2efdf4092c57dbb9e91c2853df
Emmanuel Raviart <eraviart@easter-eggs.com>
2011-08-08 00:28:33 +0200

    Increase version number to 0.8dev.

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ doc_lines = __doc__.split('\n')
 
 setup(
     name = 'Biryani',
-    version = '0.7',
+    version = '0.8dev',
 
     author = 'Emmanuel Raviart',
     author_email = 'eraviart@easter-eggs.com',
