e62c5523cc97148d8319d3db11f0f393bc068bfa
Emmanuel Raviart <eraviart@easter-eggs.com>
2011-09-07 15:25:04 +0200

    New converters for french postal code & postal routing.

--- biryani/frconv.py
+++ biryani/frconv.py
@@ -31,7 +31,11 @@ from . import strings, states
 
 __all__ = [
     'clean_str_to_phone',
+    'expand_postal_routing',
+    'shrink_postal_routing',
     'str_to_phone',
+    'str_to_postal_code',
+    'str_to_postal_routing',
     ]
 
 N_ = conv.N_
@@ -92,6 +96,63 @@ def clean_str_to_phone(value, state = states.default_state):
     return value, state._(u'Wrong number of digits in phone number')
 
 
+def expand_postal_routing(value, state = states.default_state):
+    """Replace abbreviations with their full name in a postal routing
+
+    .. note:: Postal routing must already be converted to uppercase ASCII.
+
+    >>> expand_postal_routing(u'ST NAZAIRE')
+    (u'SAINT NAZAIRE', None)
+    >>> expand_postal_routing(u'SAINT ETIENNE')
+    (u'SAINT ETIENNE', None)
+    >>> expand_postal_routing(u'STILL')
+    (u'STILL', None)
+    >>> expand_postal_routing(u'STES')
+    (u'SAINTES', None)
+    >>> expand_postal_routing(None)
+    (None, None)
+    """
+    if value is None:
+        return value, None
+    postal_routing_fragments = []
+    for word in value.split():
+        if word == 'ST':
+            word = 'SAINT'
+        elif word == 'STE':
+            word = 'SAINTE'
+        elif word == 'STES':
+            # STES is not a valid abbreviation for SAINTES, bu we accept it.
+            word = 'SAINTES'
+        postal_routing_fragments.append(word)
+    return u' '.join(postal_routing_fragments) or None, None
+
+
+def shrink_postal_routing(value, state = states.default_state):
+    """Replace words in a postal routing with their abbreviation.
+
+    .. note:: Postal routing must already be converted to uppercase ASCII.
+
+    >>> shrink_postal_routing(u'SAINT NAZAIRE')
+    (u'ST NAZAIRE', None)
+    >>> shrink_postal_routing(u'ST ETIENNE')
+    (u'ST ETIENNE', None)
+    >>> shrink_postal_routing(u'SAINTES')
+    (u'SAINTES', None)
+    >>> shrink_postal_routing(None)
+    (None, None)
+    """
+    if value is None:
+        return value, None
+    postal_routing_fragments = []
+    for word in value.split():
+        if word == 'SAINT':
+            word = 'ST'
+        elif word == 'SAINTE':
+            word = 'STE'
+        postal_routing_fragments.append(word)
+    return u' '.join(postal_routing_fragments) or None, None
+
+
 str_to_phone = conv.pipe(conv.cleanup_line, clean_str_to_phone)
 """Convert a string to a phone number.
 
@@ -103,3 +164,49 @@ str_to_phone = conv.pipe(conv.cleanup_line, clean_str_to_phone)
     >>> str_to_phone('   0123456789   ')
     (u'+33 1 23 45 67 89', None)
     """
+
+
+str_to_postal_code = conv.pipe(
+    conv.function(lambda s: strings.slugify(s, separator = u' ') or None),
+    conv.function(lambda s: u''.join(s.split())),
+    conv.test(lambda s: s.isdigit(), error = N_(u'Postal code must contain only digits')),
+    conv.function(lambda s: u'0{0}'.format(s) if len(s) == 4 else s),
+    conv.test(lambda s: len(s) == 5, error = N_(u'Postal code must have 5 digits')),
+    )
+"""Convert a string to a postal code.
+
+    >>> str_to_postal_code(u'   75014   ')
+    (u'75014', None)
+    >>> str_to_postal_code(u'   1234   ')
+    (u'01234', None)
+    >>> str_to_postal_code(u'   123   ')
+    (u'123', u'Postal code must have 5 digits')
+    >>> str_to_postal_code('   ')
+    (None, None)
+    >>> str_to_postal_code(None)
+    (None, None)
+    """
+
+
+str_to_postal_routing = conv.pipe(
+    conv.function(lambda s: strings.normalize(s, transform = strings.upper) or None),
+    shrink_postal_routing,
+    )
+"""Convert a string to a postal routing (aka locality, ie the part of the address after the postal code).
+
+    >>> str_to_postal_routing(u'   Paris  14   ')
+    (u'PARIS 14', None)
+    >>> str_to_postal_routing(u'   Paris  xiv   ')
+    (u'PARIS XIV', None)
+    >>> str_to_postal_routing(u'   SAINT Etienne   ')
+    (u'ST ETIENNE', None)
+    >>> str_to_postal_routing(u'   SAINT Étienne   ')
+    (u'ST ETIENNE', None)
+    >>> str_to_postal_routing(u'Saintes')
+    (u'SAINTES', None)
+    >>> str_to_postal_routing('   ')
+    (None, None)
+    >>> str_to_postal_routing(None)
+    (None, None)
+    """
+
