273975319a60205417703503b2798f276c4e612b
Emmanuel Raviart <eraviart@easter-eggs.com>
2012-05-28 15:37:31 +0200

    New converter func:`biryani.jsonconv.make_clean_str_to_json`.

--- biryani/jsonconv.py
+++ biryani/jsonconv.py
@@ -27,13 +27,46 @@
 
 import json
 
-from . import states
+from .baseconv import cleanup_line, pipe
+from .states import default_state
 
 
 __all__ = [
+    'make_clean_str_to_json',
     'make_json_to_str',
     'make_str_to_json',
     ]
+
+
+def make_clean_str_to_json(*args, **kwargs):
+    """Return a converter that decodes a clean string to a JSON data.
+
+    .. note:: For a converter that doesn't require a clean string, see :func:`make_str_to_json`.
+
+    >>> make_clean_str_to_json()(u'{"a": 1, "b": [2, "three"]}')
+    ({u'a': 1, u'b': [2, u'three']}, None)
+    >>> make_clean_str_to_json()(u'null')
+    (None, None)
+    >>> make_clean_str_to_json()(u'Hello World')
+    (u'Hello World', u'Invalid JSON')
+    >>> make_clean_str_to_json()(u'{"a": 1, "b":')
+    (u'{"a": 1, "b":', u'Invalid JSON')
+    >>> make_clean_str_to_json()(u'')
+    (u'', u'Invalid JSON')
+    >>> make_clean_str_to_json()(None)
+    (None, None)
+    """
+    def clean_str_to_json(value, state = default_state):
+        if value is None:
+            return value, None
+        if isinstance(value, str):
+            # Ensure that json.loads() uses unicode strings.
+            value = value.decode('utf-8')
+        try:
+            return json.loads(value, *args, **kwargs), None
+        except ValueError:
+            return value, state._(u'Invalid JSON')
+    return clean_str_to_json
 
 
 def make_json_to_str(*args, **kwargs):
@@ -52,7 +85,7 @@ def make_json_to_str(*args, **kwargs):
     >>> make_json_to_str()(None)
     (None, None)
     """
-    def json_to_str(value, state = states.default_state):
+    def json_to_str(value, state = default_state):
         if value is None:
             return value, None
         try:
@@ -66,8 +99,6 @@ def make_json_to_str(*args, **kwargs):
 def make_str_to_json(*args, **kwargs):
     """Return a converter that decodes a clean string to a JSON data.
 
-    .. note:: For a converter that doesn't require a clean string, see :func:`make_str_to_json`.
-
     >>> make_str_to_json()(u'{"a": 1, "b": [2, "three"]}')
     ({u'a': 1, u'b': [2, u'three']}, None)
     >>> make_str_to_json()(u'null')
@@ -77,18 +108,11 @@ def make_str_to_json(*args, **kwargs):
     >>> make_str_to_json()(u'{"a": 1, "b":')
     (u'{"a": 1, "b":', u'Invalid JSON')
     >>> make_str_to_json()(u'')
-    (u'', u'Invalid JSON')
+    (None, None)
     >>> make_str_to_json()(None)
     (None, None)
     """
-    def str_to_json(value, state = states.default_state):
-        if value is None:
-            return value, None
-        if isinstance(value, str):
-            # Ensure that json.loads() uses unicode strings.
-            value = value.decode('utf-8')
-        try:
-            return json.loads(value, *args, **kwargs), None
-        except ValueError:
-            return value, state._(u'Invalid JSON')
-    return str_to_json
+    return pipe(
+        cleanup_line,
+        make_clean_str_to_json(*args, **kwargs),
+        )
