dad89fc7ecb9536875571892f706ec29df6f296b
Emmanuel Raviart <eraviart@easter-eggs.com>
2012-06-28 00:56:55 +0200

    Added the 18 test vectors from NIST GCM specification.

--- biryani/gcm.py
+++ biryani/gcm.py
@@ -129,6 +129,171 @@ def main():
     tag = data[-16:]
     print repr(gcm_decrypt(key, '', ciphertext, '', tag))
 
+    # Test vectors from NIST GCM spec
+    # http://csrc.nist.gov/groups/ST/toolkit/BCM/documents/proposedmodes/gcm/gcm-spec.pdf
+
+    # Test Case 1
+    k = '00000000000000000000000000000000'.decode("hex")
+    p = ''.decode("hex")
+    iv = '000000000000000000000000'.decode("hex")
+    a = ''.decode("hex")
+    c, t = gcm_encrypt(k, iv, p, a)
+    assert c == ""
+    assert t == "58e2fccefa7e3061367f1d57a4e7455a".decode("hex")
+
+    # Test Case 2
+    k = '00000000000000000000000000000000'.decode("hex")
+    p = '00000000000000000000000000000000'.decode("hex")
+    a = ''.decode("hex")
+    iv = '000000000000000000000000'.decode("hex")
+    c, t = gcm_encrypt(k, iv, p, a)
+    assert c == "0388dace60b6a392f328c2b971b2fe78".decode("hex")
+    assert t == "ab6e47d42cec13bdf53a67b21257bddf".decode("hex")
+
+    # Test Case 3
+    k = 'feffe9928665731c6d6a8f9467308308'.decode("hex")
+    p = 'd9313225f88406e5a55909c5aff5269a86a7a9531534f7da2e4c303d8a318a721c3c0c95956809532fcf0e2449a6b525b16aedf5aa0de657ba637b391aafd255'.decode("hex")
+    a = ''.decode("hex")
+    iv = 'cafebabefacedbaddecaf888'.decode("hex")
+    c, t = gcm_encrypt(k, iv, p, a)
+    assert c == "42831ec2217774244b7221b784d0d49ce3aa212f2c02a4e035c17e2329aca12e21d514b25466931c7d8f6a5aac84aa051ba30b396a0aac973d58e091473f5985".decode("hex")
+    assert t == "4d5c2af327cd64a62cf35abd2ba6fab4".decode("hex")
+
+    # Test Case 4
+    k = 'feffe9928665731c6d6a8f9467308308'.decode("hex")
+    p = 'd9313225f88406e5a55909c5aff5269a86a7a9531534f7da2e4c303d8a318a721c3c0c95956809532fcf0e2449a6b525b16aedf5aa0de657ba637b39'.decode("hex")
+    a = 'feedfacedeadbeeffeedfacedeadbeefabaddad2'.decode("hex")
+    iv = 'cafebabefacedbaddecaf888'.decode("hex")
+    c, t = gcm_encrypt(k, iv, p, a)
+    assert c == "42831ec2217774244b7221b784d0d49ce3aa212f2c02a4e035c17e2329aca12e21d514b25466931c7d8f6a5aac84aa051ba30b396a0aac973d58e091".decode("hex")
+    assert t == "5bc94fbc3221a5db94fae95ae7121a47".decode("hex")
+
+    # Test Case 5
+    k = 'feffe9928665731c6d6a8f9467308308'.decode("hex")
+    p = 'd9313225f88406e5a55909c5aff5269a86a7a9531534f7da2e4c303d8a318a721c3c0c95956809532fcf0e2449a6b525b16aedf5aa0de657ba637b39'.decode("hex")
+    a = 'feedfacedeadbeeffeedfacedeadbeefabaddad2'.decode("hex")
+    iv = 'cafebabefacedbad'.decode("hex")
+    c, t = gcm_encrypt(k, iv, p, a)
+    assert c == "61353b4c2806934a777ff51fa22a4755699b2a714fcdc6f83766e5f97b6c742373806900e49f24b22b097544d4896b424989b5e1ebac0f07c23f4598".decode("hex")
+    assert t == "3612d2e79e3b0785561be14aaca2fccb".decode("hex")
+
+    # Test Case 6
+    k = 'feffe9928665731c6d6a8f9467308308'.decode("hex")
+    p = 'd9313225f88406e5a55909c5aff5269a86a7a9531534f7da2e4c303d8a318a721c3c0c95956809532fcf0e2449a6b525b16aedf5aa0de657ba637b39'.decode("hex")
+    a = 'feedfacedeadbeeffeedfacedeadbeefabaddad2'.decode("hex")
+    iv = '9313225df88406e555909c5aff5269aa6a7a9538534f7da1e4c303d2a318a728c3c0c95156809539fcf0e2429a6b525416aedbf5a0de6a57a637b39b'.decode("hex")
+    c, t = gcm_encrypt(k, iv, p, a)
+    assert c == "8ce24998625615b603a033aca13fb894be9112a5c3a211a8ba262a3cca7e2ca701e4a9a4fba43c90ccdcb281d48c7c6fd62875d2aca417034c34aee5".decode("hex")
+    assert t == "619cc5aefffe0bfa462af43c1699d050".decode("hex")
+
+    # Test Case 7
+    k = '000000000000000000000000000000000000000000000000'.decode("hex")
+    p = ''.decode("hex")
+    a = ''.decode("hex")
+    iv = '000000000000000000000000'.decode("hex")
+    c, t = gcm_encrypt(k, iv, p, a)
+    assert c == "".decode("hex")
+    assert t == "cd33b28ac773f74ba00ed1f312572435".decode("hex")
+
+    # Test Case 8
+    k = '000000000000000000000000000000000000000000000000'.decode("hex")
+    p = '00000000000000000000000000000000'.decode("hex")
+    a = ''.decode("hex")
+    iv = '000000000000000000000000'.decode("hex")
+    c, t = gcm_encrypt(k, iv, p, a)
+    assert c == "98e7247c07f0fe411c267e4384b0f600".decode("hex")
+    assert t == "2ff58d80033927ab8ef4d4587514f0fb".decode("hex")
+
+    # Test Case 9
+    k = 'feffe9928665731c6d6a8f9467308308feffe9928665731c'.decode("hex")
+    p = 'd9313225f88406e5a55909c5aff5269a86a7a9531534f7da2e4c303d8a318a721c3c0c95956809532fcf0e2449a6b525b16aedf5aa0de657ba637b391aafd255'.decode("hex")
+    a = ''.decode("hex")
+    iv = 'cafebabefacedbaddecaf888'.decode("hex")
+    c, t = gcm_encrypt(k, iv, p, a)
+    assert c == "3980ca0b3c00e841eb06fac4872a2757859e1ceaa6efd984628593b40ca1e19c7d773d00c144c525ac619d18c84a3f4718e2448b2fe324d9ccda2710acade256".decode("hex")
+    assert t == "9924a7c8587336bfb118024db8674a14".decode("hex")
+
+    # Test Case 10
+    k = 'feffe9928665731c6d6a8f9467308308feffe9928665731c'.decode("hex")
+    p = 'd9313225f88406e5a55909c5aff5269a86a7a9531534f7da2e4c303d8a318a721c3c0c95956809532fcf0e2449a6b525b16aedf5aa0de657ba637b39'.decode("hex")
+    a = 'feedfacedeadbeeffeedfacedeadbeefabaddad2'.decode("hex")
+    iv = 'cafebabefacedbaddecaf888'.decode("hex")
+    c, t = gcm_encrypt(k, iv, p, a)
+    assert c == "3980ca0b3c00e841eb06fac4872a2757859e1ceaa6efd984628593b40ca1e19c7d773d00c144c525ac619d18c84a3f4718e2448b2fe324d9ccda2710".decode("hex")
+    assert t == "2519498e80f1478f37ba55bd6d27618c".decode("hex")
+
+    # Test Case 11
+    k = 'feffe9928665731c6d6a8f9467308308feffe9928665731c'.decode("hex")
+    p = 'd9313225f88406e5a55909c5aff5269a86a7a9531534f7da2e4c303d8a318a721c3c0c95956809532fcf0e2449a6b525b16aedf5aa0de657ba637b39'.decode("hex")
+    a = 'feedfacedeadbeeffeedfacedeadbeefabaddad2'.decode("hex")
+    iv = 'cafebabefacedbad'.decode("hex")
+    c, t = gcm_encrypt(k, iv, p, a)
+    assert c == "0f10f599ae14a154ed24b36e25324db8c566632ef2bbb34f8347280fc4507057fddc29df9a471f75c66541d4d4dad1c9e93a19a58e8b473fa0f062f7".decode("hex")
+    assert t == "65dcc57fcf623a24094fcca40d3533f8".decode("hex")
+
+    # Test Case 12
+    k = 'feffe9928665731c6d6a8f9467308308feffe9928665731c'.decode("hex")
+    p = 'd9313225f88406e5a55909c5aff5269a86a7a9531534f7da2e4c303d8a318a721c3c0c95956809532fcf0e2449a6b525b16aedf5aa0de657ba637b39'.decode("hex")
+    a = 'feedfacedeadbeeffeedfacedeadbeefabaddad2'.decode("hex")
+    iv = '9313225df88406e555909c5aff5269aa6a7a9538534f7da1e4c303d2a318a728c3c0c95156809539fcf0e2429a6b525416aedbf5a0de6a57a637b39b'.decode("hex")
+    c, t = gcm_encrypt(k, iv, p, a)
+    assert c == "d27e88681ce3243c4830165a8fdcf9ff1de9a1d8e6b447ef6ef7b79828666e4581e79012af34ddd9e2f037589b292db3e67c036745fa22e7e9b7373b".decode("hex")
+    assert t == "dcf566ff291c25bbb8568fc3d376a6d9".decode("hex")
+
+    # Test Case 13
+    k = '0000000000000000000000000000000000000000000000000000000000000000'.decode("hex")
+    p = ''.decode("hex")
+    a = ''.decode("hex")
+    iv = '000000000000000000000000'.decode("hex")
+    c, t = gcm_encrypt(k, iv, p, a)
+    assert c == "".decode("hex")
+    assert t == "530f8afbc74536b9a963b4f1c4cb738b".decode("hex")
+
+    # Test Case 14
+    k = '0000000000000000000000000000000000000000000000000000000000000000'.decode("hex")
+    p = '00000000000000000000000000000000'.decode("hex")
+    a = ''.decode("hex")
+    iv = '000000000000000000000000'.decode("hex")
+    c, t = gcm_encrypt(k, iv, p, a)
+    assert c == "cea7403d4d606b6e074ec5d3baf39d18".decode("hex")
+    assert t == "d0d1c8a799996bf0265b98b5d48ab919".decode("hex")
+
+    # Test Case 15
+    k = 'feffe9928665731c6d6a8f9467308308feffe9928665731c6d6a8f9467308308'.decode("hex")
+    p = 'd9313225f88406e5a55909c5aff5269a86a7a9531534f7da2e4c303d8a318a721c3c0c95956809532fcf0e2449a6b525b16aedf5aa0de657ba637b391aafd255'.decode("hex")
+    a = ''.decode("hex")
+    iv = 'cafebabefacedbaddecaf888'.decode("hex")
+    c, t = gcm_encrypt(k, iv, p, a)
+    assert c == "522dc1f099567d07f47f37a32a84427d643a8cdcbfe5c0c97598a2bd2555d1aa8cb08e48590dbb3da7b08b1056828838c5f61e6393ba7a0abcc9f662898015ad".decode("hex")
+    assert t == "b094dac5d93471bdec1a502270e3cc6c".decode("hex")
+
+    # Test Case 16
+    k = 'feffe9928665731c6d6a8f9467308308feffe9928665731c6d6a8f9467308308'.decode("hex")
+    p = 'd9313225f88406e5a55909c5aff5269a86a7a9531534f7da2e4c303d8a318a721c3c0c95956809532fcf0e2449a6b525b16aedf5aa0de657ba637b39'.decode("hex")
+    a = 'feedfacedeadbeeffeedfacedeadbeefabaddad2'.decode("hex")
+    iv = 'cafebabefacedbaddecaf888'.decode("hex")
+    c, t = gcm_encrypt(k, iv, p, a)
+    assert c == "522dc1f099567d07f47f37a32a84427d643a8cdcbfe5c0c97598a2bd2555d1aa8cb08e48590dbb3da7b08b1056828838c5f61e6393ba7a0abcc9f662".decode("hex")
+    assert t == "76fc6ece0f4e1768cddf8853bb2d551b".decode("hex")
+
+    # Test Case 17
+    k = 'feffe9928665731c6d6a8f9467308308feffe9928665731c6d6a8f9467308308'.decode("hex")
+    p = 'd9313225f88406e5a55909c5aff5269a86a7a9531534f7da2e4c303d8a318a721c3c0c95956809532fcf0e2449a6b525b16aedf5aa0de657ba637b39'.decode("hex")
+    a = 'feedfacedeadbeeffeedfacedeadbeefabaddad2'.decode("hex")
+    iv = 'cafebabefacedbad'.decode("hex")
+    c, t = gcm_encrypt(k, iv, p, a)
+    assert c == "c3762df1ca787d32ae47c13bf19844cbaf1ae14d0b976afac52ff7d79bba9de0feb582d33934a4f0954cc2363bc73f7862ac430e64abe499f47c9b1f".decode("hex")
+    assert t == "3a337dbf46a792c45e454913fe2ea8f2".decode("hex")
+
+    # Test Case 18
+    k = 'feffe9928665731c6d6a8f9467308308feffe9928665731c6d6a8f9467308308'.decode("hex")
+    p = 'd9313225f88406e5a55909c5aff5269a86a7a9531534f7da2e4c303d8a318a721c3c0c95956809532fcf0e2449a6b525b16aedf5aa0de657ba637b39'.decode("hex")
+    a = 'feedfacedeadbeeffeedfacedeadbeefabaddad2'.decode("hex")
+    iv = '9313225df88406e555909c5aff5269aa6a7a9538534f7da1e4c303d2a318a728c3c0c95156809539fcf0e2429a6b525416aedbf5a0de6a57a637b39b'.decode("hex")
+    c, t = gcm_encrypt(k, iv, p, a)
+    assert c == "5a8def2f0c9e53f1f75d7853659e2a20eeb2b22aafde6419a058ab4f6f746bf40fc0c3b780f244452da3ebf1c5d82cdea2418997200ef82e44ae7e3f".decode("hex")
+    assert t == "a44a8266ee1c8eb0c8b5d4cf5ae9f19a".decode("hex")
+
 
 if __name__ == '__main__':
     main()
